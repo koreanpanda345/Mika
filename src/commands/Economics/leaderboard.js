106064cb37e7ac6552f2941ffc05fa0ac60f5f92
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2020-10-17 22:26:40 -0700

    Fixing the Leaderboard command

--- src/commands/Economics/leaderboard.js
+++ src/commands/Economics/leaderboard.js
@@ -25,7 +25,7 @@ module.exports = class LeaderboardCommand extends CommandBase
         let users = [];
         switch(this.ctx.args[0])
         {
-            case "level":
+            case "levels":
 
 
                 data.forEach(item => 
